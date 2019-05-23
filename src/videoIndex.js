ff69d664dac8d931798e7a1904efad81c848a513
Elliot Charney <elliotcharney@gmail.com>
2019-05-23 15:04:08 -0700

    update video titles

--- src/videoIndex.js
+++ src/videoIndex.js
@@ -2,92 +2,92 @@
 module.exports = [
   {
     src: "ch1.mp4",
-    title: "Chapter 1: History & Cultural Context",
+    title: "Chapter 1: History and Cultural Context",
     autoplay: true,
   },
   {
     src: "ch2.mp4",
-    title: "Chapter 2: Set of Bowls",
+    title: "Chapter 2: Bowls and Cloths",
     autoplay: true,
   },
   {
     src: "ch3.mp4",
-    title: "Chapter 3: Setting out the bowl and cloths",
+    title: "Chapter 3: Receiving Condiments & Setting Out the Bowls and Cloths",
     autoplay: true,
   },
   {
     src: "ch4.mp4",
-    title: "Chapter 4: Receiving food",
+    title: "Chapter 4: Receiving Food",
     autoplay: true,
   },
   {
     src: "ch5.mp4",
-    title: "Chapter 5: Eating forms",
+    title: "Chapter 5: Eating Forms",
     autoplay: true,
   },
   {
     src: "ch6.mp4",
-    title: "Chapter 6: Dropping utensils",
+    title: "Chapter 6: Dropping Utensils",
     autoplay: true,
   },
   {
     src: "ch7.mp4",
-    title: "Chapter 7: Receiving seconds",
+    title: "Chapter 7: Receiving Seconds",
     autoplay: true,
   },
   {
     src: "ch8.mp4",
-    title: "Chapter 8: Dry cleaning",
+    title: "Chapter 8: Dry Cleaning",
     autoplay: true,
   },
   {
     src: "ch9.mp4",
-    title: "Chapter 9: Receiving water, cleaning, and drying bowls",
+    title: "Chapter 9: Receiving Water; Cleaning, Drying Bowls",
     autoplay: true,
   },
   {
     src: "ch10.mp4",
-    title: "Chapter 10: The water offering",
+    title: "Chapter 10: The Spirit Water Offering",
     autoplay: true,
   },
   {
     src: "ch11.mp4",
-    title: "Chapter 11: Wrapping-up bowls",
+    title: "Chapter 11: Wrapping the Bowls",
     autoplay: true,
   },
   {
     src: "ch12.mp4",
-    title: "Chapter 12: Tying the knot",
+    title: "Chapter 12: Tying the Knot",
     autoplay: true,
   },
   {
     src: "ch13.mp4",
-    title: "Chapter 13: Final chant; ending the meal",
+    title: "Chapter 13: Final Chant & Ending the Meal",
     autoplay: true,
   },
   {
     src: "ch14.mp4",
-    title: "Chapter 14: Evening meal",
+    title: "Chapter 14: Evening Meal",
     autoplay: false,
   },
   {
     src: "a1.mp4",
-    title: "Appendix 1: Setting out the priest bowls",
+    title: "Appendix 1: Setting Out the Priest Bowls",
     autoplay: true
   },
   {
     src: "a2.mp4",
-    title: "Appendix 2: Wrapping up priest bowls",
+    title: "Appendix 2: Cleaning the Place Mat; Wrapping the Priest Bowls",
     autoplay: true
   },
   {
     src: "a3.mp4",
-    title: "Appendix 3: Evening meal priest bowls",
+    title: "Appendix 3: Evening Meal - Priest Bowls",
     autoplay: true
   },
   {
     src: "a4.mp4",
-    title: "Appendix 4: Priest bowls lunch spirit offering",
+    title: "Appendix 4: Lunch Spirit Offering",
     autoplay: true
   }
 ];
